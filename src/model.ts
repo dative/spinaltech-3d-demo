5ec264d0bcf909ded271b8869554ab2169f996a4
Rodrigo Passos <rodrigo@hellodative.com>
2023-06-08 22:48:52 -0400

    finish demo

--- src/model.ts
+++ src/model.ts
@@ -4,6 +4,32 @@ const braceEl = document.querySelector(
   'model-viewer#brace'
 ) as ModelViewerElement;
 
+const canvasTexture = braceEl?.createCanvasTexture();
+const canvas = canvasTexture.source.element as HTMLCanvasElement;
+canvas.width = 8000;
+canvas.height = 8000;
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+const getCanvasTexture = (image: string) => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.fillStyle = '#ffffff';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  const img = new Image();
+  img.src = image;
+
+  img.addEventListener('load', () => {
+    // Create a pattern with this image, and set it to "repeat".
+    const ptrn = ctx.createPattern(img, 'repeat') as CanvasPattern;
+    ctx.fillStyle = ptrn;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    canvasTexture.source.update();
+  });
+  return canvasTexture;
+};
+
 braceEl?.addEventListener('load', () => {
   if (braceEl?.model) {
     const bodyMaterial = braceEl.model.materials[0];
@@ -19,16 +45,20 @@ braceEl?.addEventListener('load', () => {
         );
         return;
       }
-      const texture = await braceEl.createTexture(patternUri);
-      // bodyMaterial.pbrMetallicRoughness['metallicRoughnessTexture'].setTexture(
-      //   texture
-      // );
+
+      // const texture = await braceEl.createTexture(patternUri, 'image/jpeg');
+
+      // Using a canvas texture instead of an image texture so we can set the
+      // pattern to repeat at a larger scale.
+      const texture = getCanvasTexture(patternUri);
       bodyMaterial.pbrMetallicRoughness['baseColorTexture'].setTexture(texture);
-      // bodyMaterial['normalTexture'].setTexture(texture);
-      // emissiveTexture
-      // occlusionTexture;
-      // pbrMetallicRoughness;
+
+      // Controls shine and roughness of the material
+      bodyMaterial.pbrMetallicRoughness.setMetallicFactor(0.5);
+      bodyMaterial.pbrMetallicRoughness.setRoughnessFactor(0.25);
     };
+
+    createAndApplyTexture('./patterns/TP_Lily_Rose_NL_2000x2000.jpg');
 
     const patternSelect = document?.querySelector(
       '#patterns'
