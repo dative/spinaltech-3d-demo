095230051e7729fc645aee331c7480aedcf99912
Rodrigo Passos <rodrigo@hellodative.com>
2023-06-08 20:52:10 -0400

    simplify brace

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 ## Introduction
 
 This repository contains the code for the Spinal Tech 3D Brace Experiment. The experiment is a web-based application that allows users to interact with a 3D scoliosis brace and preview different patterns. The application is built using the [Model Viewer](https://modelviewer.dev/) web component, which is a web component that allows users to view 3D models in the browser.
+
+## Notes
+
+https://hackmd.io/@XR/webxr-workflows#Blender
--- src/model.ts
+++ src/model.ts
@@ -4,23 +4,12 @@ const braceEl = document.querySelector(
   'model-viewer#brace'
 ) as ModelViewerElement;
 
-// Brace model mappings:
-// Neck Strap [0]
-// Neck Strap Pins [1]
-// exterior [2]
-// interior shield [3]
-// Strap Clip [4]
-// Strap Pins [5]
-// Straps [6]
-// Straps Clip Base [7]
-// Interior Cushion [8]
-
 braceEl?.addEventListener('load', () => {
   if (braceEl?.model) {
-    const bodyMaterial = braceEl.model.materials[2];
+    const bodyMaterial = braceEl.model.materials[0];
+    bodyMaterial.pbrMetallicRoughness.setBaseColorFactor('#FFFFFF');
 
     const createAndApplyTexture = async (patternUri: string) => {
-      // 938D84
       if (patternUri === '') {
         const texture = await braceEl.createTexture(
           './assets/Textures/Busto injoinato texturizzare_3_BaseColor.png'
@@ -31,8 +20,6 @@ braceEl?.addEventListener('load', () => {
         return;
       }
       const texture = await braceEl.createTexture(patternUri);
-
-      // bodyMaterial['normalTexture'].setTexture(texture);
       // bodyMaterial.pbrMetallicRoughness['metallicRoughnessTexture'].setTexture(
       //   texture
       // );
